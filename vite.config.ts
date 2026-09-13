39a2c1b4e700de2a034999dfa80482fa5e2f8b6c
Suresh Beekhani <sureshbeekhani26@gmail.com>
2026-09-13 19:40:19 +0500

    fix: dedupe React to resolve createContext error

--- vite.config.ts
+++ vite.config.ts
@@ -58,7 +58,10 @@ export default defineConfig(({ mode }) => ({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+      "react": path.resolve(__dirname, "./node_modules/react"),
+      "react-dom": path.resolve(__dirname, "./node_modules/react-dom"),
     },
+    dedupe: ["react", "react-dom"],
   },
   build: {
     outDir: 'dist',
